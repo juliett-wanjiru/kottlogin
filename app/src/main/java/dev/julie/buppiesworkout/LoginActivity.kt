e1a38a70c8c6b074af1caecea41c0c04188722b2
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-06-01 15:22:57 +0300

    first commit

---
+++ app/src/main/java/dev/julie/buppiesworkout/LoginActivity.kt
@@ -0,0 +1,41 @@
+package dev.julie.buppiesworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var tvsign:TextView
+    lateinit var etemail:TextView
+    lateinit var etpassword:TextView
+    lateinit var btnlogin:Button
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilpassword:TextInputLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        tvsign = findViewById(R.id.tvsign)
+        etemail=findViewById(R.id.etEmail)
+        etpassword=findViewById(R.id.etpassword)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilpassword = findViewById(R.id.tilpassword)
+        tvsign.setOnClickListener{
+            val intent=Intent(this,signupActivity::class.java)
+            startActivity(intent)
+
+        }
+       var email=etemail.text.toString()
+       var password=etpassword.text.toString()
+       if(email.isBlank()){
+           tilEmail.error="email is required"
+       }
+      if(password.isBlank()){
+            tilpassword.error="password is required" }
+}
+}
+
---
+++ app/src/main/java/dev/julie/buppiesworkout/signupActivity.kt
@@ -0,0 +1,18 @@
+package dev.julie.buppiesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class signupActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+}
