5b0570898074d1871bf94ab876da7a516d81a214
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-06-22 22:25:30 +0300

    changes

--- app/src/main/java/dev/julie/buppiesworkout/HomeActivity.kt
+++ app/src/main/java/dev/julie/buppiesworkout/HomeActivity.kt
@@ -2,11 +2,46 @@ package dev.julie.buppiesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var button_navigation:BottomNavigationView
+    lateinit var fcvHome:FragmentContainerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-    }
+        castviews()
+        setupButtonNav()
 
+
+
+    }
+fun castviews(){
+    button_navigation=findViewById(R.id.bottom_navigation)
+    fcvHome=findViewById(R.id.fcvHome)
+}
+    fun setupButtonNav(){
+         button_navigation.setOnItemSelectedListener(){item->
+            when(item.itemId){
+                R.id.plan->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome,PlanFragment()).commit()
+                    true
+                }
+                R.id.track->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome,TrackFragment()).commit()
+                    true
+                }
+                R.id.profile->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome,TrackFragment()).commit()
+                    true
+
+            }
+                else->false
+
+         }
+    }
+ }
 }
--- app/src/main/java/dev/julie/buppiesworkout/LoginActivity.kt
+++ app/src/main/java/dev/julie/buppiesworkout/LoginActivity.kt
@@ -8,29 +8,30 @@ import android.widget.TextView
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var tvsign:TextView
-    lateinit var etemail:TextView
-    lateinit var etpassword:TextView
-    lateinit var btnlogin:Button
-    lateinit var tilEmail:TextInputLayout
-    lateinit var tilpassword:TextInputLayout
-
+    lateinit var tvSignUp: TextView
+    lateinit var etEmail: TextInputLayout
+    lateinit var etPassword:TextInputLayout
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilPassword:TextInputLayout
+    lateinit var btnLogin:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        tvsign = findViewById(R.id.tvsign)
-        etemail=findViewById(R.id.etEmail)
-        etpassword=findViewById(R.id.etpassword)
-        tilEmail = findViewById(R.id.tilEmail)
-        tilpassword = findViewById(R.id.tilpassword)
-        btnlogin=findViewById(R.id.btnlogin)
-        btnlogin.setOnClickListener {
+        tvSignUp=findViewById(R.id.tvsignup)
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etpassword)
+        tilEmail=findViewById(R.id.tilEmail)
+        tilPassword=findViewById(R.id.tilpassword)
+        btnLogin=findViewById(R.id.btnLogin)
+        setContentView(R.layout.activity_login)
+
+        tvSignUp.setOnClickListener {
             val intent=Intent(this,HomeActivity::class.java)
             startActivity(intent)
 
         }
-        tvsign.setOnClickListener{
+        btnLogin.setOnClickListener{
             val intent=Intent(this,signupActivity::class.java)
             startActivity(intent)
 
